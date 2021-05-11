5ffcfe0063ed95b354bdd1ef43eeb394b383b322
Yongsik Won <youngsik3200@gmail.com>
2021-05-11 20:59:57 +0900

    들어가라

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,8 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
-import UploadProductPage from './views/UploadProductPage/UploadProductPage';
+import UploadProductPage from './views/UploadProductPage/UploadProductPage'
+import DetailProductPage from './views/DetailProductPage/DetailProductPage';
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -19,10 +20,11 @@ function App() {
       <NavBar />
       <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null)} />
+        <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
+          <Route exact path="/product/:product_Id" component={Auth(DetailProductPage, true)} />
         </Switch>
       </div>
       <Footer />
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
 
-import { useState } from 'react';
-import Item from 'antd/lib/list/Item';
 const { Title } = Typography;
 const { TextArea } = Input;
 
@@ -16,76 +16,117 @@ const Continents = [
     { key: 7, value: "Antarctica" }
 ]
 
-function UploadProductPage() {
+function UploadProductPage(props) {
 
     const [TitleValue, setTitleValue] = useState("")
-    const [DescriptionValue, setDecriptionValue] = useState("")
+    const [DescriptionValue, setDescriptionValue] = useState("")
     const [PriceValue, setPriceValue] = useState(0)
-    const [ContinentsValue, setContinentsValue] = useState(1)
+    const [ContinentValue, setContinentValue] = useState(1)
 
-    const onTitleChange = (e) =>{
-        setTitleValue(e.currentTarget.value)
+    const [Images, setImages] = useState([])
+
+
+    const onTitleChange = (event) => {
+        setTitleValue(event.currentTarget.value)
     }
-    const onDescriptionChange = (e) =>{
-        setDecriptionValue(e.currentTarget.value)
+
+    const onDescriptionChange = (event) => {
+        setDescriptionValue(event.currentTarget.value)
     }
-    const onPriceChange = (e) =>{
-        setPriceValue(e.currentTarget.value)
+
+    const onPriceChange = (event) => {
+        setPriceValue(event.currentTarget.value)
     }
-    const onContinentsSelectChange = (e) => {
-        setContinentsValue(e.currentTarget.value)
+
+    const onContinentsSelectChange = (event) => {
+        setContinentValue(event.currentTarget.value)
     }
-    
+
+    const updateImages = (newImages) => {
+        setImages(newImages)
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+
+        if (!TitleValue || !DescriptionValue || !PriceValue ||
+            !ContinentValue || !Images) {
+            return alert('fill all the fields first!')
+        }
+
+        const variables = {
+            writer: props.user.userData._id,
+            title: TitleValue,
+            description: DescriptionValue,
+            price: PriceValue,
+            images: Images,
+            continents: ContinentValue,
+        }
+
+        Axios.post('/api/product/uploadProduct', variables)
+            .then(response => {
+                if (response.data.success) {
+                    alert('Product Successfully Uploaded')
+                    props.history.push('/')
+                } else {
+                    alert('Failed to upload Product')
+                }
+            })
+
+    }
 
     return (
-        <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
-            <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
                 <Title level={2}> Upload Travel Product</Title>
             </div>
 
 
-            <form onSubmit>
+            <Form onSubmit={onSubmit} >
 
-                  {/* DropZone */}
-            <br />
-            <br />
-            <label>Title</label>
-            <Input type="text" 
+                {/* DropZone */}
+                <FileUpload refreshFunction={updateImages} />
+
+                <br />
+                <br />
+                <label>Title</label>
+                <Input
                     onChange={onTitleChange}
-                    value={TitleValue}/>
-            <br />
-            <br />
-            <label>Description</label>
-            <TextArea
-                onChange={onDescriptionChange}
-                value={DescriptionValue} />
-            <br />
-            <br />
-            <label>Price($)</label>
+                    value={TitleValue}
+                />
+                <br />
+                <br />
+                <label>Description</label>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={DescriptionValue}
+                />
+                <br />
+                <br />
+                <label>Price($)</label>
                 <Input
                     onChange={onPriceChange}
                     value={PriceValue}
                     type="number"
                 />
-            <br />
-            <br />
-            <select onChange={onContinentsSelectChange} value> 
-            {Continents.map(item => (
+                <br /><br />
+                <select onChange={onContinentsSelectChange} value={ContinentValue}>
+                    {Continents.map(item => (
                         <option key={item.key} value={item.key}>{item.value} </option>
                     ))}
-            </select>
-            <br />
-            <br />
-            <Button
-                onClick>
-                Sumbit
-            </Button>
+                </select>
+                <br />
+                <br />
 
+                <Button
+                    onClick={onSubmit}
+                >
+                    Submit
+                </Button>
 
-            </form>
+            </Form>
+
         </div>
-
-        
     )
 }
 
