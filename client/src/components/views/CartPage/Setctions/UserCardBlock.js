16a1c311dc14645b9c51d0a755a828cda46b00d6
Yongsik Won <youngsik3200@gmail.com>
2021-05-15 20:47:56 +0900

    구현 버그수정

--- client/src/components/views/CartPage/Setctions/UserCardBlock.js
+++ client/src/components/views/CartPage/Setctions/UserCardBlock.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 function UserCardBlock(props) {
 
+    // const renderCartImage = (images) =>{
+    //     if(images.length > 0){
+    //         let image = images[0]
+    //         return `http://localhost:5000/${images.image}`
+    //     }
+    // }
+
     const renderItems = () => {
         props.products && props.products.map(product => (
             <tr key={product._id}>
@@ -20,11 +27,14 @@ function UserCardBlock(props) {
                 <thead>
                     <tr>
                         <th>Product Image</th>
-                        <th>Product quantity</th>
+                        <th>Product Quantity</th>
                         <th>Product Price</th>
-                        <th>Product form Cart</th>
+                        <th>Remove from Cart</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {renderItems()}
+                </tbody>
             </table>
         </div>
     )
